6322793e34ad8853d886ec7a44282e5cd9778115
Ubuntu <ubuntu@ip-10-0-6-35.ec2.internal>
2020-04-30 13:25:47 +0000

    ugly and only goes one level down but does the job

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
-const images = "images";
+const fromFolder = "images";
+const toFolder = "tiles";
+const read = require("fs-readdir-recursive");
 
-fs.readdir(images, function(err, files) {
-  if(err) console.log(err);
-  files.forEach(function (file, index) {
-    let fileName = file.substring(0, file.indexOf("."));
-    let from = path.join(images, file);
-    let to = path.join("tiles", fileName);
-    sharp(from)
-      .tile({
-        size: 512
-      })
-      .toFile(to, function(err, info) {
-        if(err) console.log(err);
-      });
-    //console.log(file);
-    //console.log(fileName);
-  });
+const files = read(fromFolder);
+files.forEach((file, index) => {
+  let fileName = file.substring(0, file.indexOf("."));
+  let from;
+  let to;
+  let filePathArray = file.split("/");
+  if(filePathArray.length > 1) {
+    let folder = filePathArray[0];
+    fileName = filePathArray[1];
+    from = path.join(fromFolder,folder,fileName);
+    to = path.join(toFolder,folder);
+    fs.mkdirSync(to,{recursive: true});
+    to = path.join(toFolder,folder,fileName);
+  } else {
+    from = path.join(fromFolder, file);
+    to = path.join(toFolder, fileName);
+  }
+  sharp(from)
+    .tile({
+      size: 512
+    })
+    .toFile(to, function(err, info) {
+      if(err) console.log(err);
+    });
 });
+
